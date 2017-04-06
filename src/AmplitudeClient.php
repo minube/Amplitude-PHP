87ce0aa44958b6f91bdff7f7dfe9db76ae425fe3
Fran Álvarez <fjalvarez@gmail.com>
2017-04-06 16:39:40 +0200

    Adding amplitude identify API

--- src/AmplitudeClient.php
+++ src/AmplitudeClient.php
@@ -8,9 +8,13 @@ use Guzzle\Http\Client;
  */
 class AmplitudeClient implements AmplitudeClientInterface
 {
+    /** @var string */
+    const AMPLITUDE_EVENT_URL = 'https://api.amplitude.com/httpapi';
 
     /** @var string */
-    const AMPLITUDE_URL = 'https://api.amplitude.com/httpapi';
+    const AMPLITUDE_IDENTIFY_URL = 'https://api.amplitude.com/identify';
+
+    const DEFAULT_EVENT_TYPE = 'event';
 
     /**
      * @var string
@@ -47,20 +51,35 @@ class AmplitudeClient implements AmplitudeClientInterface
      */
     public function track(Message\Event $event)
     {
-        $request = $this->getClient()->post(null, null, $this->getPostBody($event));
+        $request = $this->getClient(self::AMPLITUDE_EVENT_URL)->post(
+            null, null, $this->getPostBody($event)
+        );
+        return $request->send();
+    }
+
+    /**
+     * @param Message\Event $event
+     * @return Message\Response
+     */
+    public function identify(Message\Event $event)
+    {
+        $request = $this->getClient(self::AMPLITUDE_IDENTIFY_URL)->post(
+            null, null, $this->getPostBody($event, 'identification')
+        );
         return $request->send();
     }
 
     /**
      * Get post body
      * @param Message\Event $event
+     * @param string $type
      * @return array
      */
-    protected function getPostBody(Message\Event $event)
+    protected function getPostBody(Message\Event $event, $type = self::DEFAULT_EVENT_TYPE)
     {
         return array(
             'api_key' => $this->apiKey,
-            'event' => $event->format(),
+            $type => $event->format(),
         );
     }
 
@@ -68,10 +87,10 @@ class AmplitudeClient implements AmplitudeClientInterface
      * Get client
      * @return Client
      */
-    protected function getClient()
+    protected function getClient($url)
     {
         if (null === $this->client) {
-            $this->client = new Client(self::AMPLITUDE_URL);
+            $this->client = new Client($url);
         }
         return $this->client;
     }
